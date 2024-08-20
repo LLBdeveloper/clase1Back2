7ded40a2d358febed3a94360f20349b8b77f41dc
Lautaro Bertera <bertera.lautaro@gmail.com>
2024-08-20 14:12:30 -0300

    app ok4

--- src/app.js
+++ src/app.js
@@ -4,12 +4,12 @@ import cookieParser from 'cookie-Parser'
 
 const app = express()
 const PORT = 8080
-
+const miAltaClave = "TinkiWinki"
 
 
 //Middleware
 app.use(express.json())
-app.use(cookieParser())
+app.use(cookieParser(miAltaClave))
 
 //Rutas
     //home
@@ -33,6 +33,20 @@ app.get("/setcookie", (req, res) => {
         res.clearCookie("coderCookie").send("Cookie Eliminada")
     })
 
+    //Enviamos una cookie firmada
+    app.get("/cookiefirmada", (req, res) => {
+        res.cookie("cookieFirmada", "esto es un mensaje secreto", {signed: true}).send("Cookie firmada enviada")
+    })
+
+    //Recuperamos una cookie firmada
+    app.get("/recuperamoscookiefirmada", (req, res) => {
+        let valorCookie = req.signedCookies.cookieFirmada
+        if (valorCookie) {
+            res.send("Cookie recuperada:" + valorCookie)
+        } else {
+            res.send("Cookie invalida")
+        }
+    })
 //LISTEN
 app.listen(PORT, ()=> {
     displayRoutes(app)
